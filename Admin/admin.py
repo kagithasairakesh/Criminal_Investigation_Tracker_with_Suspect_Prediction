283255b1e4fa2ba8a43936dc4cf84ac5c629042c
Shanmukha5 <shanmukhaprakash333@gmail.com>
2020-11-11 10:25:41 +0530

    created models (Case, Suspect)

--- Admin/admin.py
+++ Admin/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Admin, Officer
+from .models import Admin, Officer, Case,Criminal_Record,Suspect
 
 admin.site.register(Admin)
 admin.site.register(Officer)
+admin.site.register(Case)
+admin.site.register(Suspect)
+admin.site.register(Criminal_Record)
--- Admin/models.py
+++ Admin/models.py
@@ -12,6 +12,7 @@ class Admin(models.Model):
 	def __str__(self):
 		return str(self.name)
 
+
 class Officer(models.Model):
 	officer_id = models.IntegerField(primary_key=True)
 	name = models.CharField(max_length=40)
@@ -26,3 +27,40 @@ class Officer(models.Model):
 
 	def __str__(self):
 		return str(self.name)
+
+class Case(models.Model):
+	case_id = models.IntegerField(primary_key=True)
+	case_name = models.CharField(max_length=50)
+	case_type = models.CharField(max_length=50)
+	notes = models.CharField(max_length=500)
+	description = models.CharField(max_length=500)
+	type_of_weapon = models.CharField(max_length=255, blank=True)
+	time = models.TimeField()
+	date = models.DateField()
+	officer_id = models.ForeignKey(Officer, on_delete=models.DO_NOTHING, related_name="officers")
+	# admin_id = models.ForeignKey(Admin, on_delete=models.DO_NOTHING, related_name="admin")
+
+class Suspect(models.Model):
+	suspect_id = models.IntegerField(primary_key=True)
+	name = models.CharField(max_length=50)
+	image = models.ImageField(upload_to="images/suspects")
+	phone_number = models.CharField(max_length=12)
+	age = models.IntegerField()
+	gender = models.CharField(max_length=40)
+	relation = models.CharField(max_length=255)
+	description = models.CharField(max_length=500)
+	case_id = models.ForeignKey(Case, on_delete=models.DO_NOTHING)
+
+
+class Criminal_Record(models.Model):
+	criminal_id = models.IntegerField(primary_key=True)
+	name = models.CharField(max_length=255)
+	image = models.ImageField(upload_to="images/criminal_record")
+	phone_number = models.CharField(max_length=12)
+	age = models.IntegerField()
+	gender = models.CharField(max_length=10)
+	relation = models.CharField(max_length=255)
+	description = models.CharField(max_length=500)
+	suspect_id = models.ForeignKey(Suspect, on_delete=models.DO_NOTHING)
+	case_id = models.ForeignKey(Case, on_delete=models.DO_NOTHING)
+
